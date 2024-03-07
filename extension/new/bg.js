0bd39eb483a5d86b9fa7d1fd5197a3aa96e4ddb7
tel3port <carn3lian@gmail.com>
2024-03-07 22:16:06 +0300

    setting up the extension

--- extension/new/bg.js
+++ extension/new/bg.js
@@ -53,66 +53,38 @@ chrome.runtime.onInstalled.addListener(async function (details) {
 
     if (details.reason === "install") {
 
-        chrome.storage.local.set({ bgColor: "black" })
-        chrome.storage.local.set({ randQuote: "there is nothing so common as the wish to be remarkable : william shakespeare" })
-
-        await chrome.storage.local.get(["bgColor"])
-
     }
 
 })
 
+async function cookieChecker(cb) {
+    let res = await chrome.cookies.getAll({ url: "https://imagetotext.onrender.com/" })
+
+    if (res.length == 0) {
+
+        cb({
+            status: false,
+            message: "website not logged in"
+        })
+
+    }
+
+    else {
+        cb({
+            status: true,
+            message: "website logged in"
+        })
+    }
+}
+
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.message === "from-popup-checkauth") {
 
-    if (request.message === "from options-activate") {
-        sendResponse({
-            message: "from options activation success"
-        })
+        cookieChecker(sendResponse)
 
     }
 
-    else if (request.message === "from-popup-toggle-colors") {
-
-        const randomIndex = getRandomInt(0, colorValues.length - 1)
-
-        sendResponse({
-            message: "from popup toggle color success",
-            value: colorValues[randomIndex]
-
-        })
-    }
-
-    else if (request.message === "from-popup-change-text") {
-
-        x(sendResponse)
-
-    }
-
-    else if (request.message === "from-popup-activate-extension") {
-        //open order page for user to buy package
-
-        sendResponse({
-            message: "page opened success"
-        })
-    }
-
-    else if (request.message === "from-auth-cs.js") {
-
-        try {
-            //get the token and save it to local storage
-
-            chrome.storage.local.set({ token: request.token })
-
-            x(sendResponse)
-
-        } catch (error) {
-            console.log(error);
-
-        }
-
-
-    }
     else {
 
         try {
@@ -137,10 +109,6 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 chrome.runtime.onMessageExternal.addListener(
     function (request, sender, sendResponse) {
 
-        // console.log(request);
-        // console.log(sender);
-
-        sendResponse({ message: "external webpage communication success" })
     });
 
 
@@ -150,19 +118,3 @@ function openTab() {
         url: "popup/popup.html"
     })
 }
-
-
-async function getRandomQuote() {
-    const res1 = await fetch("https://type.fit/api/quotes")
-    const res2 = await res1.json()
-
-    return res2[getRandomInt(0, res2.length - 1)]
-
-}
-
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-const colorValues = ["red", "blue", "green", "black", "yellow", "cyan"]
--- extension/new/popup/popup.js
+++ extension/new/popup/popup.js
@@ -1,107 +1,52 @@
-'use strict';
-
-const rotateColorBtn = document.getElementById("rotateColorBtn");
-const changeTextBtn = document.getElementById("changeTextBtn");
-const accountState = document.getElementById("accountState");
-
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
-const closeModalBtn = document.querySelector(".close-modal")
-
-function toggleModalAndOverlay(messageObj) {
-    modal.classList.toggle("hidden")
-    overlay.classList.toggle("hidden")
-
-    document.getElementById("modalHeader").innerText = messageObj.heading
-    document.getElementById("modalText").innerText = messageObj.message
-    document.getElementById("accountState").innerText = messageObj.btnText
-    document.getElementById("accountState").dataset.link = messageObj.link
-
+function openTab(url) {
+    chrome.tabs.create({
+        url: url
+    })
 }
 
-closeModalBtn.addEventListener("click", toggleModalAndOverlay)
-overlay.addEventListener("click", toggleModalAndOverlay)
+(async () => {
+    let res = await chrome.runtime.sendMessage({
+        message: "from-popup-checkauth"
+    })
 
-rotateColorBtn.addEventListener("click", async function (e) {
-    try {
-        const res = await chrome.runtime.sendMessage({
-            message: "from-popup-toggle-colors"
-        })
+    if (res.status) {
+        //logged in so render the generate button
 
+        btnDivCont.innerHTML = `
+            <p>take a whole page screenhot</p>
+            <button id="screenshot">take screenshot</button>
+            `
+    }
 
-        if (res.message === "from popup toggle color success") {
-            chrome.storage.local.set({ bgColor: res.value })
-        }
+    else if (!res.status) {
+        //not logged in so render the login button
+        mainCont.innerHTML = `
+            <p>1. click the login button below</p>
+            <p>2. after login into the website</p>
+            <p>3. open this extension popup again</p>
+            <button id="login">login</button>
+            
+            `
 
-    } catch (error) {
-        console.log(error);
+        btnDivCont.remove()
 
     }
 
-})
+})()
 
-changeTextBtn.addEventListener("click", async function (e) {
 
-    try {
+document.addEventListener("click", function (e) {
+    const screenshottarget = e.target.closest("#screenshot"); // Or any other selector.
+    const logintarget = e.target.closest("#login"); // Or any other selector.
 
-        const res = await chrome.runtime.sendMessage({
-            message: "from-popup-change-text"
-        })
 
-        if (res.message === "not logged in") {
-            const obj = {
-                heading: "Premium Feature",
-                message: "You are not logged in",
-                btnText: "login",
-                link: "https://app-backend-gkbi.onrender.com/auth"
-            }
-
-            toggleModalAndOverlay(obj)
-
-        }
-
-        else if (res.message === "prcHJlbWl1bSB1c2Vy") {
-            let res = await chrome.storage.local.get(["randQuote"])
-
-            document.getElementById("textId").innerText = res
-
-        }
-
-        else if (res.message === "ZnJlZSB1c2Vyfr") {
-            let storageObj = await chrome.storage.local.get(["token"])
-
-            const obj = {
-                heading: "Premium Feature",
-                message: "please purchase a subscription to access this feature",
-                btnText: "purchase now",
-                link: `https://app-backend-gkbi.onrender.com/subscriptions/${storageObj.token}`
-            }
-
-            toggleModalAndOverlay(obj)
-
-        }
-        else {
-            const obj = {
-                heading: "problem with token",
-                message: res.message,
-                btnText: "log in",
-                link: `https://app-backend-gkbi.onrender.com/auth`
-            }
-
-            toggleModalAndOverlay(obj)
-        }
-    } catch (error) {
-        console.log(error);
-
+    if (screenshottarget) {
+        openTab("https://imagetotext.onrender.com/")
     }
 
-
-})
-
-
-accountState.addEventListener("click", function (e) {
-
-    window.open(`${accountState.getAttribute("data-link")}`, "_blank")
+    else if (logintarget) {
+        openTab("https://imagetotext.onrender.com/login")
+    }
 
 
 })
