a30d9baabbbfb8373c4c70a8e78b6376d84a192b
tel3port <carn3lian@gmail.com>
2024-03-10 01:16:27 +0300

    taking full screenshots and making appropriate responses

--- extension/new/bg.js
+++ extension/new/bg.js
@@ -3,6 +3,7 @@ let accountState = false
 chrome.runtime.onInstalled.addListener(async function (details) {
 
     if (details.reason === "install") {
+        //open a page showcasing how app works and provides early bird access
 
     }
 
@@ -11,8 +12,6 @@ chrome.runtime.onInstalled.addListener(async function (details) {
 async function cookieChecker(cb) {
     // let res = await chrome.cookies.getAll({ url: "https://imagetotext-lper.onrender.com/" })
     let res = await chrome.cookies.getAll({ url: "http://127.0.0.1:3000/" })
-
-    console.log("cookies arr: ", res);
 
     if (res.length == 0) {
 
@@ -40,7 +39,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     }
 
     else if (request.message === "from-popup-account-status") {
-        //hit the api here to find out account status
+        //hit the api here to find out account status (free or paid)
 
         if (accountState) {
             sendResponse({ status: true })
@@ -49,6 +48,19 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
             sendResponse({ status: false })
 
         }
+    }
+
+    else if (request.message === "from-popup-wp") {
+        chrome.tabs.captureVisibleTab(null, { format: "png" }, function (screenshotUrl) {
+            console.log("taken? ", screenshotUrl.startsWith("data:image/png;base64"));
+
+            if (screenshotUrl.startsWith("data:image/png;base64")) {
+
+                sendResponse({ message: "screenshot taken", payload: screenshotUrl })
+            }
+
+        })
+
     }
 
     else {
@@ -71,11 +83,6 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
 })
 
-//does not work with my subdomain. Works with localhost tho
-chrome.runtime.onMessageExternal.addListener(
-    function (request, sender, sendResponse) {
-
-    });
 
 
 //another way of open a url in a new tab
--- extension/new/popup/popup.js
+++ extension/new/popup/popup.js
@@ -1,4 +1,5 @@
 const mainCont = document.getElementById("main")
+const parentCont = document.getElementById("parent")
 const btnDivCont = document.getElementById("btnDiv");
 
 const modal = document.querySelector(".modal");
@@ -33,8 +34,6 @@ function openTab(url) {
         message: "from-popup-checkauth"
     })
 
-    console.log("res===>", res);
-
     if (res.status) {
         //logged in so render the generate button
 
@@ -68,19 +67,75 @@ function openTab(url) {
 
 
 document.addEventListener("click", function (e) {
-    const wp_screenshottarget = e.target.closest("#wp_screenshot"); // Or any other selector.
-    const cus_screenshottarget = e.target.closest("#cus_screenshot"); // Or any other selector.
-    const logintarget = e.target.closest("#login"); // Or any other selector.
-
-
-
+    const wp_screenshottarget = e.target.closest("#wp_screenshot"); // whole page ss
+    const custom_screenshottarget = e.target.closest("#cus_screenshot");// custom ss
+    const notarget = e.target.closest("#no");
+    const logintarget = e.target.closest("#login");
 
 
     if (wp_screenshottarget) {
-        // take screenshot and open in new tab
+        // take screenshot, open in new tab send it as post request for processing
+
+        chrome.runtime.sendMessage({ message: "from-popup-wp" }, (res) => {
+            console.log("wp res:", res);
+
+            if (res.message === "screenshot taken") {
+                //rewrite the dom to show appropriate message
+                //post request with image url data
+
+                parentCont.innerHTML = `
+                <div id="main" class="activationContent">
+                <div id="msg">
+                    <h3>Processing the screenshot.</h3>
+                    <h3>Please be patient...</h3>
+                </div>
+                <div class="boxes">
+                    <div class="box">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div class="box">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div class="box">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                    </div>
+                    <div class="box">
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div> </div>
+                    </div>
+                </div>
+    
+    
+            </div>
+                
+                `
+
+
+            }
+            else {
+                console.log("something went wrong!");
+            }
+            // let image = new Image();
+            // image.src = res.payload;
+
+            // let w = window.open("");
+            // w.document.write(image.outerHTML);
+        })
+
 
     }
-    else if (cus_screenshottarget) {
+    else if (custom_screenshottarget) {
         /**
  * check if is premium user if not, show modal
  * if free user, if no_thanks is clicked then show button
@@ -107,6 +162,11 @@ document.addEventListener("click", function (e) {
 
 
 
+    }
+
+    else if (notarget) {
+        toggleModalAndOverlay({})
+        console.log("take the screenshot and open it in new tab!");
     }
 
     else if (logintarget) {
