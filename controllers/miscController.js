c4bd9e37f4121852567b1e85b3287296aad2698c
tel3port <carn3lian@gmail.com>
2024-03-11 03:22:18 +0300

    corrected text extraction code

--- controllers/miscController.js
+++ controllers/miscController.js
@@ -1,11 +1,15 @@
 
+const path = require("path")
 const tes = require("tesseract.js");
 
 exports.extractText = (request, response, next) => {
     try {
         (async () => {
+            const image = path.resolve(__dirname, ('../public/assets/img/backgrounds/test1.png'));
+
             const worker = await tes.createWorker('eng');
-            const ret = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
+            // const ret = await worker.recognize('/public/assets/img/backgrounds/test1.png');
+            const ret = await worker.recognize(image);
             console.log(ret.data.text);
             await worker.terminate();
         })();
